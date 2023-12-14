d3b681d80623a04b9250d10eb4c760f906561d45
hkorzeniewski <hubert.korzeniewski@theengineers.tech>
2023-12-14 17:43:33 +0100

    async tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,10 +1,32 @@
 import pytest_asyncio
 from httpx import AsyncClient
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.orm import sessionmaker
+from sqlmodel import SQLModel, create_engine
 
 from app.main import app
 
+DATABASE_URL = "sqlite:///./test.db"  # Use a separate test database
+engine = create_engine(DATABASE_URL)
+
+
+@pytest_asyncio.fixture(scope="function")
+async def async_session() -> AsyncSession:
+    session = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
+
+    async with session() as s:
+        async with engine.begin() as conn:
+            await conn.run_sync(SQLModel.metadata.create_all)
+
+        yield s
+
+    async with engine.begin() as conn:
+        await conn.run_sync(SQLModel.metadata.drop_all)
+
+    await engine.dispose()
+
 
 @pytest_asyncio.fixture
-async def fastapi_client() -> AsyncClient:
+async def fastapi_client():
     async with AsyncClient(app=app, base_url="http://test") as client:
         yield client
--- tests/test_api.py
+++ tests/test_api.py
@@ -1,45 +1,8 @@
-from fastapi.testclient import TestClient
-
-from app.main import app
-
-client = TestClient(app)
+import pytest
+from httpx import AsyncClient
 
 
-def test_get_currency_data():
-    response = client.get("/currencies/select_one?currency_code=eur_pln")
+@pytest.mark.asyncio
+async def test_get_currencies(fastapi_client: AsyncClient):
+    response = await fastapi_client.get("/currencies")
     assert response.status_code == 200
-    assert "rate_date" in response.json()[0]
-    assert "eur_pln" in response.json()[0]
-
-
-def test_get_many_currency_data():
-    response = client.get("/currencies/select_many?selected_columns=eur_pln&selected_columns=usd_pln")
-    assert response.status_code == 200
-    assert "rate_date" in response.json()[0]
-    assert "eur_pln" in response.json()[0]
-    assert "usd_pln" in response.json()[0]
-
-
-def test_get_average_currency_rate():
-    response = client.get("/currencies/eur_pln/average")
-    assert response.status_code == 200
-    assert "avarage for eur_pln" in response.json()
-
-
-def test_get_maximum_currency_rate():
-    response = client.get("/currencies/eur_pln/maximum")
-    assert response.status_code == 200
-    assert "maximum for eur_pln" in response.json()
-
-
-def test_get_minimum_currency_rate():
-    response = client.get("/currencies/eur_pln/minimum")
-    assert response.status_code == 200
-    assert "minimum for eur_pln" in response.json()
-
-
-def test_get_currency_rate_date_range():
-    response = client.get("/currencies/eur_pln/date-range?start_date=2023-11-01&end_date=2023-12-12")
-    assert response.status_code == 200
-    assert "rate_date" in response.json()[0]
-    assert "eur_pln" in response.json()[0]
